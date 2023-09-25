420446f7d419de4080ee242b25d6bf108f523d6f
Krishna Bhatt <me@balancerator.com>
2023-09-24 18:15:20 -0700

    Add the main page

--- app/src/main/java/com/balancerator/MainActivity.java
+++ app/src/main/java/com/balancerator/MainActivity.java
@@ -2,13 +2,26 @@ package com.balancerator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btPickerBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.btPickerBtn = findViewById(R.id.connectBtn);
+        this.btPickerBtn.setOnClickListener(this::showBTPicker);
+    }
+
+    public void showBTPicker(View view) {
+        Toast.makeText(getApplicationContext(), "Clicked the button", Toast.LENGTH_SHORT).show();
     }
 }
